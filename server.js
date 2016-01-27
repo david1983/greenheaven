46e70273b2602a20654cfb02bc0044cbca55e0e4
davide andreazzini <davide andreazzini>
2016-01-27 23:49:43 +0000

    added cookie policy

--- server.js
+++ server.js
@@ -14,26 +14,21 @@ var smtpConfig = {
         rejectUnauthorized: false
     }
 };
-
-
 var nodemailer = require('nodemailer');
 // create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport(smtpConfig);
-
 app.use(express.static(__dirname + '/public'));
-
-
-
 app.post('/api/send_query',function(req,res){
-
 	console.log(req.body)
 	if( typeof req.body == 'undefined') { return res.json({error: "body is undefined"})}
 	// setup e-mail data with unicode symbols
 	var mailOptions = {
-	    from: req.body.first_name + ' ' + req.body.last_name +' 👥 <' + req.body.email + '>', // sender address
+	    from: req.body.first_name + 
+	    ' ' + req.body.last_name +' 👥 <' + req.body.email + '>', // sender address
 	    to: 'green.heaven.london@gmail.com', // list of receivers
 	    subject: 'Contact from Green Heaven ✔', // Subject line
-	    text: "User: " + req.body.first_name + " " + req.body.last_name + " " + req.body.email + "\nMessage: " + req.body.message, // plaintext body	    
+	    text: "User: " + req.body.first_name + " " + req.body.last_name + 
+	    " " + req.body.email + "\nMessage: " + req.body.message, // plaintext body	    
 	};
 
 	// send mail with defined transport object
@@ -43,10 +38,7 @@ app.post('/api/send_query',function(req,res){
 	    }
 	    return res.json(info)
 	});
-
 })
-
-
 app.listen(3111, function () {
-  console.log('Example app listening on port 3111!');
+  console.log('Green Heaven app listening on port 3111!');
 })
